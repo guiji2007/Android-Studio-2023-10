0ded579efdbccbbf904d9e1b3644035aa6f2541a
guiji2007 <98149176+guiji2007@users.noreply.github.com>
2023-10-22 11:43:10 -0500

    My second Commit

--- app/src/main/java/com/zybooks/newasprojectingithub/MainActivity.java
+++ app/src/main/java/com/zybooks/newasprojectingithub/MainActivity.java
@@ -2,13 +2,41 @@ package com.zybooks.newasprojectingithub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView textView;
+    EditText editText;
+    Button btnClick;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+        editText = findViewById(R.id.editext);
+        btnClick = findViewById(R.id.btnClick);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editTXT = editText.getText().toString();
+
+                textView.setText("" + editTXT);
+            }
+        });
+
+
+
     }
 }
